e5a51b0f1fbb7567858eb6a8f9252b717f68543b
tipgame <jensburkhardt@web.de>
2022-11-06 12:29:42 +0100

    new ui

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4d6bf5b8f6fe2fb8b13c5ca04a92f545",
-"index.html": "b233aa024dda14d90204e607d7b67dde",
-"/": "b233aa024dda14d90204e607d7b67dde",
+"index.html": "9dccbb5314c13a27f1fdd406caa2cdc8",
+"/": "9dccbb5314c13a27f1fdd406caa2cdc8",
 "main.dart.js": "fa990a6b7d9386fdf78052f84e075ebc",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
